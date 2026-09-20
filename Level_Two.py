bec54d9c7f76cae2b764d1b96fe1fc0837955729
Sujan Maharjan <sujanmaharjan1d@gmail.com>
2026-09-20 15:10:08 +0545

    duplicated id done

--- Level_Two.py
+++ Level_Two.py
@@ -9,19 +9,22 @@ class Record:
     city: str
     score: str
 
-    def is_valid(self):
-            errors= self.get_errors()
+    def is_valid(self,duplicate_id=False):
+        errors= self.get_errors(duplicate_id)
 
-            if len(errors)==0:
-                return True
-            else:
-                return False
+        if len(errors)==0:
+            return True
+        else:
+            return False
 
-    def get_errors(self):
+    def get_errors(self,duplicate_id=False):
         errors= []
 
         if self.id.strip()=="":
             errors.append("id is empty")
+
+        if duplicate_id:
+            errors.append("id is duplicate")
 
         if self.name.strip()=="":
             errors.append("name is empty")
@@ -61,30 +64,39 @@ with open('messy_people.csv','r') as file:
 
 valid_records= []
 invalid_records= []
+duplicate_count= 0
 seen_ids= set()
 
 for i in data:
     obj= Record(i["id"], i["name"], i["age"], i["city"], i["score"])
-    
-    if obj.id in seen_ids:
-        print("Duplicate id=",obj.id)
-    else:
-        seen_ids.add(obj.id)
 
-    if obj.is_valid():
+    if obj.id.strip()!="":
+
+        if obj.id in seen_ids:
+            duplicate_id= True
+            duplicate_count+= 1
+        else:
+            duplicate_id= False
+            seen_ids.add(obj.id)
+
+    else:
+        duplicate_id= False
+
+    if obj.is_valid(duplicate_id):
         valid_records.append(obj)
     else:
-        invalid_records.append(obj)
+        invalid_records.append((obj,duplicate_id))
         
 
 print("Valid Records=",len(valid_records))
 print("Invalid Records=",len(invalid_records))
+print("Duplicate Ids=",duplicate_count)
 
 print("\nValid records are:")
 for record in valid_records:
     print(record)
 
 print("\nInvalid records are:")
-for record in invalid_records:
+for record, duplicate_id in invalid_records:
     print(record)
-    print(record.get_errors())
+    print(record.get_errors(duplicate_id))
