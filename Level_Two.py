9bb42d673a25c4cc92f38ba689cbd292178d1584
Sujan Maharjan <sujanmaharjan1d@gmail.com>
2026-09-23 16:46:09 +0545

    updated

--- Level_Two.py
+++ Level_Two.py
@@ -1,5 +1,5 @@
 print("LEVEL TWO LOADED")
-print("__name__ =", __name__)
+print("LEVEL TWO __name__ =", __name__)
 from words_to_num import word_into_number
 from dataclasses import dataclass
 import csv
